87511983227189ddfd63b7c08b47660872e6f3d0
NICO <annicolas2@gmail.com>
2025-08-06 09:41:05 -0500

    Cleared app file

--- src/app.js
+++ src/app.js
@@ -11,30 +11,3 @@ app.use('/api', router)
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
-
-// app.get('/', async (req, res) => {
-//     res.status(200).send('<h1>Mi super página web</h1>')
-// })
-
-// app.get('/users', async (req, res) => {
-//     try {
-//         const users = await prisma.user.findMany()
-//         res.json(users)
-//     } catch (error) {
-//         res.status(500).json({ error: 'Failed conection to data base'})
-//     }
-// })
-
-// app.post('/users', async (req, res) => {
-//     try {
-//         const newUser = await prisma.user.create({
-//             data: req.body
-//         })
-
-//         console.log(newUser)
-
-//         res.status(201).json(newUser)
-//     } catch (error) {
-//         res.status(500).json({ error: 'Failed conection to data base'})
-//     }
-// })
